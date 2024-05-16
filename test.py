5da9b9b85eaf81c6e0dfc67b242506a752398d2a
jamiklaucich <james.miklaucich@gmail.com>
2024-05-16 16:01:46 +0100

    a

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import clr
 import sys
 import time
 import pyvisa as visa
-#
+#wad
 rm = visa.ResourceManager()
 rm.list_resources()
 keithley_2450 = rm.open_resource("USB0::0x05E6::0x2450::04131844::0::INSTR")
