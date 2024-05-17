9eb5c8db002d292f8f92ba1e6c1b7ad448d5f9a7
jamiklaucich <70892046+jamiklaucich@users.noreply.github.com>
2024-05-17 10:10:14 +0100

    k2450 visa

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ import pyvisa as visa
 #wadasdawd
 rm = visa.ResourceManager()
 rm.list_resources()
-keithley_2450 = rm.open_resource("USB0::0x05E6::0x2450::04131844::0::INSTR")
+keithley_2450 = rm.open_resource("USB0::0x05E6::0x2450::04610529::0::INSTR")
